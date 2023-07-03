18294fb6f3f5f8f759636d319ebd641121cf2388
Pandicon <70060103+Pandicon@users.noreply.github.com>
2023-07-03 13:21:26 +0200

    Split the coordinate lines into separate files

--- sphere/lines_generator.js
+++ sphere/lines_generator.js
@@ -1,16 +1,30 @@
-let res = '';
-for (let i = 0; i < 360; i += 5) {
-	for (let j = -90.0; j < 90.0; j += 5) {
-		let width = j == 0 ? 2 : 1;
-		let colour = j == 0 ? 'D90D0D23' : 'D9620D10';
-		res += `${i}.0,${j}.0,${i + 5}.0,${j}.0,${colour},${width}.0\n`;
+const template = 'ra_start,dec_start,ra_end,dec_end,colour,width';
+console.log('');
+let res = `celestial-meridians.csv\n${template}\n`;
+for (let ra = 0; ra < 360; ra += 5) {
+	for (let de = -90.0; de < 90.0; de += 5) {
+		if (de == 0) continue;
+		res += `${ra}.0,${de}.0,${(ra + 5) % 360}.0,${de}.0,D9620D10,1.0\n`;
 	}
 }
-for (let i = 0; i < 360; i += 5) {
-	for (let j = -90.0; j < 90.0; j += 5) {
-		let width = i == 0 || i == 180 ? 2 : 1;
-		let colour = i == 0 || i == 180 ? 'D90D0D23' : 'D9620D10';
-		res += `${i}.0,${j}.0,${i}.0,${j + 5}.0,${colour},${width}.0\n`;
+
+res += `\nprime-meridian.csv\n${template}\n`;
+for (let ra = 0; ra < 360; ra += 180) {
+	for (let de = -90.0; de < 90.0; de += 5) {
+		res += `${ra}.0,${de}.0,${ra}.0,${(de + 5) % 360}.0,D90D0D23,2.0\n`;
 	}
+}
+
+res += `\ncelestial-lines-of-latitude.csv\n${template}\n`;
+for (let ra = 0; ra < 360; ra += 5) {
+	if (ra == 0 || ra == 180) continue;
+	for (let de = -90.0; de < 90.0; de += 5) {
+		res += `${ra}.0,${de}.0,${ra}.0,${(de + 5) % 360}.0,D9620D10,1.0\n`;
+	}
+}
+
+res += `\ncelestial-equator.csv\n${template}\n`;
+for (let ra = 0; ra < 360; ra += 5) {
+	res += `${ra}.0,0.0,${(ra + 5) % 360}.0,0.0,D90D0D23,2.0\n`;
 }
 console.log(res);
