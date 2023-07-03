ea68bc2c6bc8c8b8eb1b4f9706aebe668fd07cda
Pandicon <70060103+Pandicon@users.noreply.github.com>
2023-07-03 11:36:48 +0200

    Move the projection into separate functions

--- src/rendering/caspr.rs
+++ src/rendering/caspr.rs
@@ -8,6 +8,30 @@ const STARS_FOLDER: &str = "./sphere/stars";
 #[path = "../geometry.rs"]
 mod geometry;
 
+fn get_point_vector(ra: f32, dec: f32, rotation_matrix: Matrix3<f32>) -> Vector3<f32> {
+	let (ra_s, ra_c) = ((-ra) * PI / 180.0).sin_cos();
+	let (de_s, de_c) = ((90.0 - dec) * PI / 180.0).sin_cos();
+	rotation_matrix * Vector3::new(de_s * ra_c, de_s * ra_s, de_c)
+}
+
+fn project_point(vector: &Vector3<f32>, zoom: f32, viewport_rect: egui::Rect) -> (egui::Pos2, bool) {
+	let scale_factor = 1.0 - vector[2] / zoom;
+
+	let rect_size = Vector2::new(viewport_rect.max[0] - viewport_rect.min[0], viewport_rect.max[1] - viewport_rect.min[1]);
+
+	let screen_ratio = 2.0 / (rect_size[0] * rect_size[0] + rect_size[1] * rect_size[1]).sqrt();
+
+	let point_coordinates = Vector2::new(vector[0] / scale_factor, vector[1] / scale_factor);
+
+	(
+		egui::Pos2::new(point_coordinates[0] / screen_ratio + rect_size[0] / 2.0, point_coordinates[1] / screen_ratio + rect_size[1] / 2.0),
+		// Is it within the bounds that we want to render in? //TODO: Use the geometry::is_in_rect function
+		// TODO: Probably fix this - see how it is rendering into the top panel
+		((rect_size[0] * screen_ratio / 2.0 > point_coordinates[0]) && (point_coordinates[0] > -rect_size[0] * screen_ratio / 2.0))
+			|| ((rect_size[1] * screen_ratio / 2.0 > point_coordinates[1]) && (point_coordinates[1] > -rect_size[1] * screen_ratio / 2.0)),
+	)
+}
+
 #[derive(Clone, Copy, Deserialize)]
 pub struct Star {
 	pub ra: f32,
@@ -26,10 +50,7 @@ pub struct StarRaw {
 
 impl Star {
 	pub fn get_renderer(&self, rotation_matrix: Matrix3<f32>) -> StarRenderer {
-		let (ra_s, ra_c) = ((-self.ra) * PI / 180.0).sin_cos(); // + rotation_ra
-		let (de_s, de_c) = ((90.0 - self.dec) * PI / 180.0).sin_cos();
-		let unit_vector = rotation_matrix * Vector3::new(de_s * ra_c, de_s * ra_s, de_c);
-		StarRenderer::new(unit_vector, self.vmag, self.colour)
+		StarRenderer::new(get_point_vector(self.ra, self.dec, rotation_matrix), self.vmag, self.colour)
 	}
 
 	pub fn from_raw(raw_star: StarRaw, default_colour: Color32) -> Self {
@@ -99,26 +120,10 @@ pub struct CellestialSphere {
 impl CellestialSphere {
 	//Renders a circle based on its current normal (does NOT account for the rotation of the sphere)
 	pub fn render_circle(&self, normal: &Vector3<f32>, radius: f32, color: eframe::epaint::Color32, painter: &egui::Painter) {
-		let scale_factor = 1.0 - normal[2] / self.zoom;
+		let (projected_point, is_within_bounds) = project_point(normal, self.zoom, self.viewport_rect);
 
-		let viewport_rect = self.viewport_rect;
-
-		let rect_size = Vector2::new(viewport_rect.max[0] - viewport_rect.min[0], viewport_rect.max[1] - viewport_rect.min[1]);
-
-		let screen_ratio = 2.0 / (rect_size[0] * rect_size[0] + rect_size[1] * rect_size[1]).sqrt();
-
-		let point_coordinates = Vector2::new(normal[0] / scale_factor, normal[1] / scale_factor);
-
-		// Is it within the bounds that we want to render in? //TODO: Use the geometry::is_in_rect function
-		// TODO: Probably fix this - see how it is rendering into the top panel
-		if ((rect_size[0] * screen_ratio / 2.0 > point_coordinates[0]) && (point_coordinates[0] > -rect_size[0] * screen_ratio / 2.0))
-			|| ((rect_size[1] * screen_ratio / 2.0 > point_coordinates[1]) && (point_coordinates[1] > -rect_size[1] * screen_ratio / 2.0))
-		{
-			painter.circle_filled(
-				egui::Pos2::new(point_coordinates[0] / screen_ratio + rect_size[0] / 2.0, point_coordinates[1] / screen_ratio + rect_size[1] / 2.0),
-				radius,
-				color,
-			);
+		if is_within_bounds {
+			painter.circle_filled(projected_point, radius, color);
 		}
 	}
 
