58fb6598621d149c3e32cf16127e0179ae249797
Matym1 <84038762+Matym1@users.noreply.github.com>
2023-07-04 23:13:01 +0200

    Removed duplicate questions.

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,17 +1,17 @@
 use rand::Rng;
-use std::{collections::HashMap, error::Error, fs};
-
 use super::caspr::CellestialSphere;
 
 
 pub enum Question{
     ObjectQuestion {name:String, ra:f32,dec:f32},
-    PositionQuestion {ra:f32,dec:f32}
+    PositionQuestion {ra:f32,dec:f32},
+    ThisPointObject {name:String, ra:f32,dec:f32}
 }
 
 pub struct GameHandler{
     current_question:usize,
     questions:Vec<Question>,
+    used_questions:Vec<usize>
 }
 
 impl GameHandler {
@@ -25,7 +25,7 @@ impl GameHandler {
                     None => {
                         match &deepsky.caldwell{
                             None => {name = String::from("Fuck");},
-                            Some(_name) => {name=String::from(_name)}
+                            Some(_name) => {name=String::from(format!("C {}",_name))}
                         }
                     }
                     Some(_name)=>{name=String::from(_name)}
@@ -34,19 +34,28 @@ impl GameHandler {
             }
         }
 
-        Self { current_question: rand::thread_rng().gen_range(0..catalog.len()), questions: catalog}
+        Self { current_question: rand::thread_rng().gen_range(0..catalog.len()), questions: catalog,used_questions:Vec::new()}
 		
 
 	}
 
     pub fn next_question(&mut self){
-        self.current_question = rand::thread_rng().gen_range(0..self.questions.len())
+
+        self.used_questions.push(self.current_question);
+        let mut possible_questions:Vec<usize> =Vec::new();
+        for question in 0..self.questions.len(){
+            if !self.used_questions.contains(&question){
+                possible_questions.push(question);
+            }
+        }
+        self.current_question=possible_questions[rand::thread_rng().gen_range(0..possible_questions.len())]
 
     }
     pub fn get_display_question(&self)-> String{
         match &self.questions[self.current_question]{
             Question::ObjectQuestion{name,ra:_ra,dec:_dec} => {return String::from(format!("Find {}.",name));}
-            Question::PositionQuestion { ra:_ra, dec:_dec } => {return String::from("This does not work yet...");}
+            Question::PositionQuestion { ra:_ra, dec:_dec } => {return String::from("This does not work yet...Sorry :)");}
+            Question::ThisPointObject { name:_name, ra:_ra, dec:_dec } => {return  String::from("What is this object?");}
         }
     }
 }
