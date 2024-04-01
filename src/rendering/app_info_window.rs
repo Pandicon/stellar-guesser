31e866e65841be5fc76a8fd9653b74c6464d3826
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-04-01 19:54:26 +0200

    Fix function naming
    The function names for the statistics and app info windows were swapped

--- src/rendering/app_info_window.rs
+++ src/rendering/app_info_window.rs
@@ -1,7 +1,7 @@
 use crate::Application;
 
 impl Application {
-	pub fn render_statistics_window(&mut self, ctx: &egui::Context) -> Option<egui::InnerResponse<Option<()>>> {
+	pub fn render_application_info_window(&mut self, ctx: &egui::Context) -> Option<egui::InnerResponse<Option<()>>> {
 		egui::Window::new("Application info").open(&mut self.state.windows.app_info.opened).show(ctx, |ui| {
 			ui.label(format!("Authors: {}", self.authors));
 			ui.label(format!("Version: {}", self.version));
--- src/rendering/stats_window.rs
+++ src/rendering/stats_window.rs
@@ -1,7 +1,7 @@
 use crate::Application;
 
 impl Application {
-	pub fn render_application_info_window(&mut self, ctx: &egui::Context) -> Option<egui::InnerResponse<Option<()>>> {
+	pub fn render_statistics_window(&mut self, ctx: &egui::Context) -> Option<egui::InnerResponse<Option<()>>> {
 		let seconds_spent = self.state.time_spent_start + (self.frame_timestamp - self.state.start_timestamp);
 		let mut time_spent_changing = seconds_spent;
 		let seconds = time_spent_changing % 60;
