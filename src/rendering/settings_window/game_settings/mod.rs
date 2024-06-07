2a1bca4ded13938fdaa9bc093704693ee2e4a0cd
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-06-07 17:27:38 +0200

    Expand the content to the width of the window
    At least visually - the scroll bar is now on the right side of the window, not somewhere in the middle, depending on the window width

--- src/rendering/settings_window/game_settings/mod.rs
+++ src/rendering/settings_window/game_settings/mod.rs
@@ -25,11 +25,13 @@ impl Application {
             );
         });
         ui.separator();
-        egui::ScrollArea::vertical().show(ui, |ui| match self.state.windows.settings.game_settings.subwindow {
-            GameSettingsSubWindow::General => self.render_game_settings_general_subwindow(ui),
-            GameSettingsSubWindow::Questions => self.render_game_settings_questions_subwindow(ui, &mut tolerance_changed),
-            GameSettingsSubWindow::Constellations => self.render_game_settings_constellations_subwindow(ui),
-        });
+        egui::ScrollArea::vertical()
+            .auto_shrink([false, true])
+            .show(ui, |ui| match self.state.windows.settings.game_settings.subwindow {
+                GameSettingsSubWindow::General => self.render_game_settings_general_subwindow(ui),
+                GameSettingsSubWindow::Questions => self.render_game_settings_questions_subwindow(ui, &mut tolerance_changed),
+                GameSettingsSubWindow::Constellations => self.render_game_settings_constellations_subwindow(ui),
+            });
         if tolerance_changed && self.game_handler.show_tolerance_marker() {
             let markers = self.game_handler.generate_player_markers(&self.game_handler.guess_marker_positions);
             self.cellestial_sphere.markers.insert("game".to_string(), markers);
