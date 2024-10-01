4517ec0a28c6eff6d417f5bdf32eae8be3ba411f
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-10-01 22:41:53 +0200

    Add a function for clamped arc x circle intersections

--- src/geometry/spherical/great_circle_arc.rs
+++ src/geometry/spherical/great_circle_arc.rs
@@ -67,6 +67,52 @@ impl GreatCircleArc {
         )
     }
 
+    /// Returns the intersections of the arc with the great circle, clamped to the arc. So if there are no intersections, the endpoint closest to the potential intersections (of the great circle and the arc extended into a great circle) is returned.
+    pub fn intersect_great_circle_clamped(&self, other: &GreatCircle) -> Result<Vec<SphericalPoint>, SphericalError> {
+        let normal1 = self.start.cartesian().cross(&self.end.cartesian());
+        let normal2 = other.start().cartesian().cross(&other.end().cartesian());
+
+        let res = normal1.cross(&normal2);
+        if res.magnitude_squared() < VEC_LEN_IS_ZERO.powi(2) {
+            return Err(SphericalError::IdenticalGreatCircles);
+        }
+        let res_norm = res.normalize();
+        let point_1 = SphericalPoint::from_cartesian_vector3(res_norm);
+        let point_2 = SphericalPoint::from_cartesian_vector3(-res_norm);
+
+        #[cfg(test)]
+        dbg!(point_1);
+        #[cfg(test)]
+        dbg!(point_2);
+
+        let mut intersections = Vec::new();
+        if self.contains_point(&point_1) {
+            intersections.push(point_1);
+        }
+        if self.contains_point(&point_2) {
+            intersections.push(point_2);
+        }
+        if intersections.is_empty() {
+            let start_1_distance = self.start.minus_cotan_distance(&point_1);
+            let start_2_distance = self.start.minus_cotan_distance(&point_2);
+            let start_distance = start_1_distance.min(start_2_distance);
+            let end_1_distance = self.end.minus_cotan_distance(&point_1);
+            if end_1_distance < start_distance {
+                intersections.push(self.end);
+            } else {
+                let end_2_distance = self.end.minus_cotan_distance(&point_2);
+                if end_2_distance < start_distance {
+                    intersections.push(self.end);
+                } else {
+                    intersections.push(self.start);
+                }
+            }
+        }
+        Ok(
+            intersections
+        )
+    }
+
     pub fn contains_point(&self, point: &SphericalPoint) -> bool {
         let tolerance: f32 = 10e-5;
         let great_circle = GreatCircle::from_arc(&self);
@@ -156,10 +202,45 @@ mod tests {
         let intersections_3 = arc_3.intersect_great_circle(&circle_3).expect("The circles are not parallel");
         assert_eq!(intersections_3.len(), 1);
         assert!(intersections_3[0].approximately_equals(&SphericalPoint::new(0.0, 0.0), tolerance));
+
         let arc_4 = GreatCircleArc::new(SphericalPoint::new(PI / 5.0,  -PI / 7.0), SphericalPoint::new(PI / 2.0, PI / 4.0)).expect("The points are far enough");
         let circle_4 = GreatCircle::new(SphericalPoint::new(PI / 5.0, PI / 4.0), SphericalPoint::new(PI / 5.0, -PI / 4.0)).expect("The points are far enough");
         let intersections_4 = arc_4.intersect_great_circle(&circle_4).expect("The circles are not parallel");
         assert_eq!(intersections_4.len(), 1);
         assert!(intersections_4[0].approximately_equals(&SphericalPoint::new(PI / 5.0,  -PI / 7.0), tolerance));
+    }
+
+    #[test]
+    fn intersect_great_circle_clamped() {
+        let tolerance = 10e-4;
+        let arc_1 = GreatCircleArc::new(SphericalPoint::new(0.0, PI / 4.0), SphericalPoint::new(0.0, -PI / 4.0)).expect("The points are far enough");
+        let circle_1 = GreatCircle::new(SphericalPoint::new(PI / 4.0, 0.0), SphericalPoint::new(-PI / 4.0, 0.0)).expect("The points are far enough");
+        let intersections_1 = arc_1.intersect_great_circle_clamped(&circle_1).expect("The circles are not parallel");
+        assert_eq!(intersections_1.len(), 1);
+        assert!(intersections_1[0].approximately_equals(&SphericalPoint::new(0.0, 0.0), tolerance));
+
+        let arc_2 = GreatCircleArc::new(SphericalPoint::new(0.0, PI / 6.0), SphericalPoint::new(0.0, PI / 4.0)).expect("The points are far enough");
+        let circle_2 = GreatCircle::new(SphericalPoint::new(PI / 4.0, 0.0), SphericalPoint::new(-PI / 4.0, 0.0)).expect("The points are far enough");
+        let intersections_2 = arc_2.intersect_great_circle_clamped(&circle_2).expect("The circles are not parallel");
+        assert_eq!(intersections_2.len(), 1);
+        assert!(intersections_2[0].approximately_equals(&SphericalPoint::new(0.0, PI / 6.0), tolerance));
+
+        let arc_3 = GreatCircleArc::new(SphericalPoint::new(0.0, 0.0), SphericalPoint::new(PI / 2.0, PI / 4.0)).expect("The points are far enough");
+        let circle_3 = GreatCircle::new(SphericalPoint::new(PI / 4.0, 0.0), SphericalPoint::new(-PI / 4.0, 0.0)).expect("The points are far enough");
+        let intersections_3 = arc_3.intersect_great_circle_clamped(&circle_3).expect("The circles are not parallel");
+        assert_eq!(intersections_3.len(), 1);
+        assert!(intersections_3[0].approximately_equals(&SphericalPoint::new(0.0, 0.0), tolerance));
+
+        let arc_4 = GreatCircleArc::new(SphericalPoint::new(PI / 5.0,  -PI / 7.0), SphericalPoint::new(PI / 2.0, PI / 4.0)).expect("The points are far enough");
+        let circle_4 = GreatCircle::new(SphericalPoint::new(PI / 5.0, PI / 4.0), SphericalPoint::new(PI / 5.0, -PI / 4.0)).expect("The points are far enough");
+        let intersections_4 = arc_4.intersect_great_circle_clamped(&circle_4).expect("The circles are not parallel");
+        assert_eq!(intersections_4.len(), 1);
+        assert!(intersections_4[0].approximately_equals(&SphericalPoint::new(PI / 5.0,  -PI / 7.0), tolerance));
+
+        let arc_5 = GreatCircleArc::new(SphericalPoint::new(PI / 5.0, PI / 7.0), SphericalPoint::new(PI / 2.0, PI / 6.0)).expect("The points are far enough");
+        let circle_5 = GreatCircle::new(SphericalPoint::new(PI / 2.0 + PI / 5.0, 0.0), SphericalPoint::new(PI / 2.0 + PI / 5.0, PI / 4.0)).expect("The points are far enough");
+        let intersections_5 = arc_5.intersect_great_circle_clamped(&circle_5).expect("The circles are not parallel");
+        assert_eq!(intersections_5.len(), 1);
+        assert!(intersections_5[0].approximately_equals(&SphericalPoint::new(PI / 2.0, PI / 6.0), tolerance));
     }
 }
