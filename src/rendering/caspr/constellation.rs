185b5421d5a3a3f9e874e991811626050616e664
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-10-18 21:48:45 +0200

    Store polygons of constellations
    Verticies are no longer stored since they are useless in two ways:
    1) They can be extracted from the polygons
    2) They do not work in the constellation Serpens, which consists of two polygons

--- src/rendering/caspr/constellation.rs
+++ src/rendering/caspr/constellation.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-use spherical_geometry::SphericalPoint;
+use spherical_geometry::{Polygon, SphericalPoint};
 
 #[derive(Clone, Deserialize)]
 pub struct BorderVertex {
@@ -24,7 +24,7 @@ pub struct Constellation {
     pub abbreviation: String,
     /// \[abbreviation, latin name, ...\]
     pub possible_names: Vec<String>,
-    pub vertices: Vec<SphericalPoint>,
+    pub polygons: Vec<Polygon>
 }
 
 impl Constellation {
@@ -34,7 +34,7 @@ impl Constellation {
             Self {
                 abbreviation: abbreviation.clone(),
                 possible_names: vec![abbreviation.clone(), raw.name_latin],
-                vertices: Vec::new(),
+                polygons: Vec::new()
             },
             abbreviation,
         )
--- src/rendering/testing_window.rs
+++ src/rendering/testing_window.rs
@@ -21,41 +21,28 @@ impl Application {
             if prev_selected_constellation != self.testing_settings.highlight_stars_in_constellation {
                 match self.cellestial_sphere.constellations.get_mut(&self.testing_settings.highlight_stars_in_constellation) {
                     Some(constellation) => {
-                        let constellation_vertices = constellation
-                            .vertices
-                            .iter()
-                            .map(|v| {
-                                println!("{} {}", v.ra(), v.dec());
-                                spherical_geometry::SphericalPoint::new(v.ra() * PI / 180.0, v.dec() * PI / 180.0)
-                            })
-                            .collect::<Vec<spherical_geometry::SphericalPoint>>();
-                        match spherical_geometry::Polygon::new(constellation_vertices, spherical_geometry::EdgeDirection::CounterClockwise) {
-                            Ok(polygon) => {
-                                println!("{} polygon acquired: {:#?}", self.testing_settings.highlight_stars_in_constellation, polygon.vertices());
-                                for category in self.cellestial_sphere.stars.values_mut() {
-                                    println!("Stars in category: {:?}", category.len());
-                                    for star in category {
-                                        // println!("{} {}", star.ra, star.dec);
-                                        let pos = spherical_geometry::SphericalPoint::new(star.ra * PI / 180.0, star.dec * PI / 180.0);
-                                        match polygon.contains_point(&pos) {
-                                            Ok(v) => {
-                                                // println!("{}", v);
-                                                if v {
-                                                    star.colour = egui::Color32::GREEN;
-                                                } else {
-                                                    star.colour = egui::Color32::WHITE;
-                                                }
+                        for category in self.cellestial_sphere.stars.values_mut() {
+                            println!("Stars in category: {:?}", category.len());
+                            'stars: for star in category {
+                                // println!("{} {}", star.ra, star.dec);
+                                let pos = spherical_geometry::SphericalPoint::new(star.ra * PI / 180.0, star.dec * PI / 180.0);
+                                for polygon in &constellation.polygons {
+                                    match polygon.contains_point(&pos) {
+                                        Ok(v) => {
+                                            // println!("{}", v);
+                                            if v {
+                                                star.colour = egui::Color32::GREEN;
+                                                continue 'stars;
+                                            } else {
+                                                star.colour = egui::Color32::WHITE;
                                             }
-                                            Err(_) => {
-                                                println!("Could not determine the constellation for star at dec={} ra={}", star.dec, star.ra);
-                                                star.colour = egui::Color32::RED;
-                                            }
+                                        }
+                                        Err(_) => {
+                                            println!("Could not determine the constellation for star at dec={} ra={}", star.dec, star.ra);
+                                            star.colour = egui::Color32::RED;
                                         }
                                     }
                                 }
-                            }
-                            Err(_) => {
-                                println!("Failed to create the polygon for the {} constellation", self.testing_settings.highlight_stars_in_constellation);
                             }
                         }
                     }
