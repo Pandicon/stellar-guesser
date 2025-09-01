e599ed604fc7d6f30b056b9600d0f11efd7f2ae8
Pandicon <70060103+Pandicon@users.noreply.github.com>
2025-09-01 21:29:10 +0200

    Target the latest SDK version (35) and use gradle 8.6.0

--- app/gen/android/app/build.gradle.kts
+++ app/gen/android/app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
     namespace="com.github.noreply.users.stellar_guesser"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.github.noreply.users.stellar_guesser"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
