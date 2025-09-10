52dad82cca37496da8f83afb05a0be597e8bda75
Pandicon <70060103+Pandicon@users.noreply.github.com>
2025-09-10 22:47:20 +0200

    Extract rendering of inner elements into a separate function

--- app/src/rendering/feedback_and_help_window.rs
+++ app/src/rendering/feedback_and_help_window.rs
@@ -4,10 +4,16 @@ use crate::Application;
 
 impl Application {
     pub fn render_feedback_and_support_window(&mut self, ctx: &egui::Context) -> Option<egui::InnerResponse<Option<()>>> {
-        egui::Window::new("Provide feedback and get support").open(&mut self.state.windows.feedback_and_help.opened).show(ctx, |ui| {
-            ui.label("We would love to get some feedback from you! Whether you have encountered an issue, want to suggest a new feature, need help with the app or learning the sky in general, or just want to join our community and have a chat, you can join us at the following places:");
-            ui.heading("Discord server");
-            ui.hyperlink(&crate::CONFIG.discord_server_invite);
-        })
+        egui::Window::new("Provide feedback and get support")
+            .open(&mut self.state.windows.feedback_and_help.opened)
+            .show(ctx, |ui| {
+                render_feedback_and_support_window_inner(ui);
+            })
     }
+}
+
+pub fn render_feedback_and_support_window_inner(ui: &mut egui::Ui) {
+    ui.label("We would love to get some feedback from you! Whether you have encountered an issue, want to suggest a new feature, need help with the app or learning the sky in general, or just want to join our community and have a chat, you can join us at the following places:");
+    ui.heading("Discord server");
+    ui.hyperlink(&crate::CONFIG.discord_server_invite);
 }
