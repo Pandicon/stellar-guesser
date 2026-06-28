64f0be9c6097be398a4daa4a915f4eb8d0163d8e
Pandicon <70060103+Pandicon@users.noreply.github.com>
2026-06-28 19:09:40 +0200

    Make most of the generic structs immutable

--- app/src/game/game_handler.rs
+++ app/src/game/game_handler.rs
@@ -20,34 +20,34 @@ pub const QUESTION_PACK_QUESTIONS_DIV: &str = "&||&";
 pub const QUESTION_PACK_QUESTIONS_PARTS_DIV: &str = "&|&";
 
 pub struct QuestionWindowData<'a> {
-    pub cellestial_sphere: &'a mut CellestialSphere,
-    pub sky: &'a mut sky::Sky,
+    pub cellestial_sphere: &'a CellestialSphere,
+    pub sky: &'a sky::Sky,
     pub theme: &'a Theme,
     pub game_question_opened: &'a mut bool,
-    pub request_input_focus: &'a mut bool,
-    pub add_marker_on_click: &'a mut bool,
+    pub request_input_focus: &'a bool,
+    pub add_marker_on_click: &'a bool,
     pub question_number_text: &'a String,
-    pub game_stage: &'a mut GameStage,
+    pub game_stage: &'a GameStage,
     pub ctx: &'a eframe::egui::Context,
-    pub score: &'a mut u32,
-    pub possible_score: &'a mut u32,
+    pub score: &'a u32,
+    pub possible_score: &'a u32,
     pub is_scored_mode: bool,
     pub current_question: usize,
-    pub used_questions: &'a mut Vec<usize>,
+    pub used_questions: &'a Vec<usize>,
     pub questions_settings: &'a questions::Settings,
-    pub question_number: &'a mut usize,
+    pub question_number: &'a usize,
 }
 
 pub struct QuestionCheckingData<'a> {
-    pub sky: &'a mut sky::Sky,
+    pub sky: &'a sky::Sky,
     pub theme: &'a Theme,
-    pub game_stage: &'a mut GameStage,
+    pub game_stage: &'a GameStage,
     pub is_scored_mode: bool,
     pub current_question: usize,
-    pub used_questions: &'a mut Vec<usize>,
-    pub add_marker_on_click: &'a mut bool,
+    pub used_questions: &'a Vec<usize>,
+    pub add_marker_on_click: &'a bool,
     pub questions_settings: &'a questions::Settings,
-    pub question_number: &'a mut usize,
+    pub question_number: &'a usize,
 }
 
 pub trait QuestionTrait {
